c4a371eba1969306dededb8b8d57cb8b482a76e7
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-20 16:18:47 +0800

    Create Calculator class in main.js
    Populated such with all methods already in main.js.

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
-function add(a, b) {
-    /**Returns sum of a and b.*/
-    return a + b;
+class Calculator {
+    add(a, b) {
+        /**Returns sum of a and b.*/
+        return a + b;
+    }
+    
+    subtract(a, b) {
+        /**Returns difference of a and b.*/
+        return a - b;
+    }
+    
+    multiply(a, b) {
+        /**Returns product of a and b.*/
+        return a * b;
+    }
+    
+    divide(a, b) {
+        /**Returns quotient of a and b.*/
+        return a / b;
+    }
+
+    operate(operator, a, b) {
+        /**Returns return value of operator fed with arguments a and b.*/
+        return operator(a, b);
+    }
 }
 
-function subtract(a, b) {
-    /**Returns difference of a and b.*/
-    return a - b;
-}
-
-function multiply(a, b) {
-    /**Returns product of a and b.*/
-    return a * b;
-}
-
-function divide(a, b) {
-    /**Returns quotient of a and b.*/
-    return a / b;
-}
-
-function operate(operator, a, b) {
-    /**Returns return value of operator fed with arguments a and b.*/
-    return operator(a, b);
-}
+let calculator = new Calculator;
+console.log(calculator.divide(1, 12))
