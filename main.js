26479d9291ba17746ad7ce828cc6b837821700cf
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-20 17:40:54 +0800

    Configure delete button
    Clicking it now removes one character from display window.

--- main.js
+++ main.js
@@ -33,27 +33,39 @@ class UserInterface {
 
     trackClearButton() {
         /**Adds event listener to #calc-clear-btn div. When such is clicked it
-         * now empties the calculator's display window. */
-        let clearButton = document.getElementById('calc-clear-btn');
-        clearButton.addEventListener("click", () => {
-            this.calcDisplay.textContent = "";
+         * empties the calculator's display window. */
+        const clearButton = document.getElementById('calc-clear-btn');
+        clearButton.addEventListener('click', () => {
+            this.calcDisplay.textContent = '';
+        });
+    }
+
+    trackDeleteButton() {
+        /**Adds event listener to #calc-delete-btn div. If clicked it removes
+         * one character from calculator's display window. */
+        const deleteButton = document.getElementById('calc-delete-btn');
+        deleteButton.addEventListener('click', () => {
+            const oldText = this.calcDisplay.textContent;
+            const newText = oldText.substring(0, oldText.length - 1);
+            this.calcDisplay.textContent = newText;
         });
     }
 
     trackInputButtons() {
         /**Adds event listener to .calc-input-btn divs. Such divs now add
          * their text content to #calc-display div when clicked.*/
-        let inputButtons = document.querySelectorAll(".calc-input-btn");
+        const inputButtons = document.querySelectorAll('.calc-input-btn');
         for (let i = 0; i < inputButtons.length; i++) {
-            inputButtons[i].addEventListener("click", () => {
+            inputButtons[i].addEventListener('click', () => {
                 this.calcDisplay.textContent += inputButtons[i].textContent;
             });
         }
     }
 
     setUp() {
-        /**Calls all functions in UserInterface class.*/
+        /**Calls all methods in UserInterface class.*/
         this.trackClearButton();
+        this.trackDeleteButton();
         this.trackInputButtons();
     }
 }
