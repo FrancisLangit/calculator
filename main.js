200950a7c22fab64001cab3775ffe2aa48a74421
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-21 18:30:12 +0800

    Create setUpEqualsButton and setUpOperatorButtons
    - Methods currently allow buggy calculation to be done.
    - Current bug disallows 2nd operand to have more than one digit.

--- main.js
+++ main.js
@@ -20,8 +20,26 @@ class Calculator {
     }
 
     operate(operator, a, b) {
-        /**Returns return value of operator fed with arguments a and b.*/
-        return operator(a, b);
+        /**Returns value after operating on a and b dependent on operator.*/
+        a = parseFloat(a);
+        b = parseFloat(b);
+        
+        let result;
+        switch(operator) {
+            case '+':
+                result = this.add(a, b);
+                break;
+            case '-':
+                result = this.subtract(a, b);
+                break;
+            case '*':
+                result = this.multiply(a, b);
+                break;
+            case '/':
+                result = this.divide(a, b);
+                break;
+        }
+        return result;
     }
 }
 
@@ -29,9 +47,13 @@ class UserInterface {
     constructor() {
         this.calculator = new Calculator;
         this.calcDisplay = document.getElementById('calc-display'); 
+        
+        this.operator = '';
+        this.firstNum = '';
+        this.secondNum = '';
     }
 
-    trackClearButton() {
+    setUpClearButton() {
         /**Adds event listener to #calc-clear-btn div. When such is clicked it
          * empties the calculator's display window. */
         const clearButton = document.getElementById('calc-clear-btn');
@@ -40,7 +62,7 @@ class UserInterface {
         });
     }
 
-    trackDeleteButton() {
+    setUpDeleteButton() {
         /**Adds event listener to #calc-delete-btn div. If clicked it removes
          * one character from calculator's display window. */
         const deleteButton = document.getElementById('calc-delete-btn');
@@ -51,22 +73,57 @@ class UserInterface {
         });
     }
 
-    trackInputButtons() {
+    setUpInputButtons() {
         /**Adds event listener to .calc-input-btn divs. Such divs now add
          * their text content to #calc-display div when clicked.*/
         const inputButtons = document.querySelectorAll('.calc-input-btn');
         for (let i = 0; i < inputButtons.length; i++) {
             inputButtons[i].addEventListener('click', () => {
-                this.calcDisplay.textContent += inputButtons[i].textContent;
+                const input = inputButtons[i].textContent
+
+                if (this.firstNum === '') {
+                    this.calcDisplay.textContent += input;
+                } else if (this.firstNum !== '') {
+                    this.calcDisplay.textContent = '';
+                    this.calcDisplay.textContent += input;
+                }
             });
         }
     }
 
+    setUpOperatorButtons() {
+        const operatorButtons = document.querySelectorAll(
+            '.calc-operator-btn');
+        for (let i = 0; i < operatorButtons.length; i++) {
+            operatorButtons[i].addEventListener('click', () => {
+                if (this.firstNum === '') {
+                    this.operator = operatorButtons[i].textContent;
+                    this.firstNum = this.calcDisplay.textContent;
+                    console.log(this.firstNum, this.operator);
+                }
+            });
+        }
+    }
+
+    setUpEqualsButton() {
+        const equalsButton = document.getElementById("calc-equals-btn");
+        equalsButton.addEventListener('click', () => {
+            if (this.firstNum !== '' && this.secondNum === '') {
+                this.secondNum = this.calcDisplay.textContent;
+                let result = this.calculator.operate(
+                    this.operator, this.firstNum, this.secondNum);
+                this.calcDisplay.textContent = result; 
+            }
+        });
+    }
+
     setUp() {
         /**Calls all methods in UserInterface class.*/
-        this.trackClearButton();
-        this.trackDeleteButton();
-        this.trackInputButtons();
+        this.setUpClearButton();
+        this.setUpDeleteButton();
+        this.setUpInputButtons();
+        this.setUpOperatorButtons();
+        this.setUpEqualsButton();
     }
 }
 
