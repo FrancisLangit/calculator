6fa6f2626cd92b5b280e97d446bf30f3e3877dd6
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-25 17:16:22 +0800

    Refactor display window code

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,25 @@
 import Calculator from "./calculator.js";
 
 import ColorButtons from "./objects/colorButtons.js";
+import DeletionButtons from "./objects/deletionButtons.js";
+import Display from "./objects/display.js";
 import KeyboardSupport from "./objects/keyboardSupport.js";
+
 
 class Main {
     constructor() {
-        this.calculator = new Calculator;
+        // this.display = document.querySelectorAll('.calc-display')[0];
 
+        this.calculator = new Calculator;
         this.colorButtons = new ColorButtons;
+        this.display = new Display(this);
+        this.deletionButtons = new DeletionButtons(this);
+
         this.keyboardSupport = new KeyboardSupport;
-        this.calcDisplay = document.getElementsByClassName('calc-display')[0];
-        
+
         this.operator = '';
         this.firstNum = '';
         this.waitingForSecondNum = false;
-    }
-
-    resetCalcDisplay() {
-        /**Resets calcDisplay text content as well as its font-size and 
-         * margin-top CSS attributes to default values.*/
-        this.calcDisplay.textContent = "0";
-        this.calcDisplay.style.fontSize = "75px";
-        this.calcDisplay.style.marginTop = "100px";
-    }
-
-    setUpAllClearButton() {
-        /**Adds event listener to .calc-ac-btn div. When such is clicked it
-         * empties the calculator's display window and resets this.operator,
-         * this.firstNum and this.waitingForSecondNum to default values.*/
-        const allClearButton = document.getElementsByClassName(
-            'calc-ac-btn')[0];
-        allClearButton.addEventListener('click', () => {
-            this.resetCalcDisplay();
-            this.operator = '';
-            this.firstNum = '';
-            this.waitingForSecondNum = false;
-        });
-    }
-
-    setUpClearButton() {
-        /**Adds event listener to .calc-c-btn div. If clicked it removes
-         * one character from calculator's display window if such not just
-         * displaying 0. */
-        const clearButton = document.getElementsByClassName('calc-c-btn')[0];
-        clearButton.addEventListener('click', () => {
-            if (this.calcDisplay.textContent.length <= 1) {
-                this.resetCalcDisplay();
-            } else if (this.calcDisplay.textContent !== '0') {
-                const oldNum = this.calcDisplay.textContent;
-                const newNum = oldNum.substring(0, oldNum.length - 1);
-                this.calcDisplay.textContent = newNum;
-            }
-        });
     }
 
     setUpPercentButton() {
@@ -61,37 +29,10 @@ class Main {
         const percentButton = document.getElementsByClassName(
             'calc-percent-btn')[0];
         percentButton.addEventListener('click', ()  => {
-            const oldNum = parseFloat(this.calcDisplay.textContent);
+            const oldNum = parseFloat(this.display.div.textContent);
             const newNum = oldNum / 100;
-            this.calcDisplay.textContent = newNum;
+            this.display.div.textContent = newNum;
         });
-    }
-
-    formatDisplayNumber() {
-        /**Formats display number to exponential notation if and when its 
-         * length goes above 10 characters.*/
-        const calcDisplayText = this.calcDisplay.textContent;
-        if (calcDisplayText.length >= 10) {
-            let newNum = Number.parseFloat(calcDisplayText).toExponential(3);
-            this.calcDisplay.textContent = newNum;
-        }
-    }
-
-    formatDisplayWindow() {
-        /**Makes font size of display window div smaller when its text content
-         * gets too large. Also adjusts top margin of such accordingly.*/
-        this.formatDisplayNumber();
-
-        const calcDisplayStyles = getComputedStyle(this.calcDisplay);
-        const calcDisplayFontSize = parseFloat(
-            calcDisplayStyles.getPropertyValue('font-size'));
-        const calcDisplayMarginTop = parseFloat(
-            calcDisplayStyles.getPropertyValue('margin-top'));
-
-        if (this.calcDisplay.clientWidth > 335) {
-            this.calcDisplay.style.fontSize = calcDisplayFontSize - 7.5 * 2;
-            this.calcDisplay.style.marginTop = calcDisplayMarginTop + 9 * 2;
-        } 
     }
 
     canAppendNum(inputChar) {
@@ -99,12 +40,12 @@ class Main {
          * - User tries to input multiple decimal points. 
          * - textContent of display already at 16 characters,
          * - textContent of display equal to 'Infinity'.*/
-        const calcDisplayText = this.calcDisplay.textContent;
+        const displayText = this.display.div.textContent;
 
         const isMultiDecimal = (
-            inputChar === '.' && calcDisplayText.split(".").length >= 2);
-        const isOverMaxChars = (calcDisplayText.length > 16);
-        const isInfinity = (calcDisplayText === 'Infinity');
+            inputChar === '.' && displayText.split(".").length >= 2);
+        const isOverMaxChars = (displayText.length > 16);
+        const isInfinity = (displayText === 'Infinity');
 
         return (!isMultiDecimal && !isOverMaxChars && !isInfinity);
     }
@@ -113,12 +54,12 @@ class Main {
         /**Appends digit or decimal inputted by user to current number in 
          * display window. Only does so if canAppendNum === true.*/
         if (this.canAppendNum(inputChar)) {
-            let newNum = this.calcDisplay.textContent + inputChar;
+            let newNum = this.display.div.textContent + inputChar;
             if (newNum[0] === '0' && newNum.length >= 2) {
                 newNum = newNum.replace(/^0/, '');
             }
-            this.calcDisplay.textContent = newNum;
-            this.formatDisplayWindow();
+            this.display.div.textContent = newNum;
+            this.display.formatDiv();
         }
     }
 
@@ -130,7 +71,7 @@ class Main {
         for (let i = 0; i < inputButtons.length; i++) {
             inputButtons[i].addEventListener('click', () => {
                 if (this.waitingForSecondNum) {
-                    this.calcDisplay.textContent = '';
+                    this.display.div.textContent = '';
                     this.waitingForSecondNum = false;
                 }
                 this.appendNum(inputButtons[i].textContent);
@@ -141,12 +82,12 @@ class Main {
     displayResult() {
         /**Calculates result of user input and displays such in calculator
          * display window.*/
-        this.calcDisplay.textContent = this.calculator.operate(
+        this.display.div.textContent = this.calculator.operate(
             this.operator,
             parseFloat(this.firstNum),
-            parseFloat(this.calcDisplay.textContent),
+            parseFloat(this.display.div.textContent),
         );
-        this.formatDisplayWindow();
+        this.display.formatDiv();
     }
 
     setUpOperatorButtons() {
@@ -162,7 +103,7 @@ class Main {
                     this.displayResult();
                 }
                 this.operator = operatorButtons[i].textContent;
-                this.firstNum = this.calcDisplay.textContent;
+                this.firstNum = this.display.div.textContent;
                 this.waitingForSecondNum = true;
             });
         }
@@ -184,10 +125,9 @@ class Main {
     setUp() {
         /**Calls methods in class associated with setting up user interface.*/
         this.colorButtons.setUp();
+        this.deletionButtons.setUp();
         this.keyboardSupport.setUp();
         
-        this.setUpAllClearButton();
-        this.setUpClearButton();
         this.setUpPercentButton();
         this.setUpInputButtons();
         this.setUpOperatorButtons();
